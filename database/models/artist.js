740ea6d347184bc3f378b4e465793b48095f6757
Ni Tongyang <ntyaiqingr@gmail.com>
2017-02-09 22:54:02 -0500

    modify the schema with mongodb data structure

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,20 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const AblumSchema = require('./album');
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AblumSchema]
+});
+
+const ArtistSchema = mongoose.model('artist', ArtistSchema);
+
+module.exports = ArtistSchema;
